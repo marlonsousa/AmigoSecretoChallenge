4e5a10a8416f6ca827e6b34f9c5c354e29e8aac2
Marlon Sousa <marllon.sousa@hotmail.com>
2025-03-10 17:37:37 -0300

    funcao para atualizar a lista com os nomes na tela

--- app.js
+++ app.js
@@ -2,24 +2,36 @@
 
 let listaAmigos = []
 let entradaNomeAmigo
-
+let exibirNomeAmigo
 
 function limparEntrada(){
     entrada = document.querySelector('input');
     entrada.value = '';
 }
 
+function atualizarLista() {
+    const ul = document.getElementById("listaAmigos");
+    ul.innerHTML = ""; // Limpa a lista antes de recriá-la
+    
+    listaAmigos.forEach(amigo => {
+        const li = document.createElement("li");
+        li.textContent = amigo;
+        ul.appendChild(li);
+    });
+}
 
 function adicionarAmigo(){
     entradaNomeAmigo = document.querySelector('input')
     if (entradaNomeAmigo.value == "" ){
         alert("Por favor, insira um nome.")
+        limparEntrada()
     }else {
         listaAmigos.push(entradaNomeAmigo.value)
         limparEntrada()
         console.log(listaAmigos)
         listaExibicao = document.getElementById('listaAmigos')
         listaExibicao.value= listaAmigos
+        atualizarLista()
     }
     
 
